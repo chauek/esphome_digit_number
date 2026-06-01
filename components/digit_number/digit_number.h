7c906801b2728026f05ff452c758e20f14040b01
Chauek <chauek@gmail.com>
2026-06-01 15:20:30 +0200

    feat: add burst/trigger fields and TriggerMeasurementAction to header

--- components/digit_number/digit_number.h
+++ components/digit_number/digit_number.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define DIGIT_NUMBER_VERSION "1.1.3"
+#define DIGIT_NUMBER_VERSION "1.2.1"
 
 #include <array>
 #include <memory>
@@ -10,6 +10,8 @@
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/camera/camera.h"
 #include "esphome/components/esp32_camera/esp32_camera.h"
+#include "esphome/core/automation.h"
+#include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace digit_number {
@@ -41,6 +43,11 @@ class DigitNumber : public sensor::Sensor, public Component, public camera::Came
   void set_display_off_threshold(uint8_t t) { display_off_threshold_ = t; }
   void set_update_interval(uint32_t ms) { update_interval_ms_ = ms; }
   void set_paused(bool p) { paused_ = p; }
+  void set_trigger_pin(GPIOPin *pin) { trigger_pin_ = pin; }
+  void set_burst_count(uint8_t count) { burst_count_ = count; }
+  void set_burst_trigger_interval(uint32_t ms) { burst_trigger_interval_ms_ = ms; }
+  void set_burst_rest_duration(uint32_t ms) { burst_rest_duration_ms_ = ms; }
+  void do_trigger_();
 
   void setup() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
@@ -55,6 +62,8 @@ class DigitNumber : public sensor::Sensor, public Component, public camera::Came
   int8_t decode_digit_(uint8_t bitmask) const;
   void process_image_();
   void publish_all_(const char *state);
+  void burst_tick_();
+  void wait_for_ok_();
 
   esp32_camera::ESP32Camera *camera_{nullptr};
   sensor::Sensor *staleness_sensor_{nullptr};
@@ -67,12 +76,39 @@ class DigitNumber : public sensor::Sensor, public Component, public camera::Came
   uint32_t update_interval_ms_{5000};
   uint32_t last_publish_ms_{0};
   bool paused_{false};
+  // trigger pin
+  GPIOPin *trigger_pin_{nullptr};
+
+  // burst config
+  uint8_t  burst_count_{3};
+  uint32_t burst_trigger_interval_ms_{10000};
+  uint32_t burst_rest_duration_ms_{300000};
+
+  // burst runtime state
+  uint8_t  burst_read_count_{0};
+  bool     burst_resting_{false};
+  uint32_t burst_rest_start_ms_{0};
+  bool     trigger_busy_{false};
+  int      wait_ok_remaining_{0};
+
+  // last_state string for trigger logic
+  std::string last_state_str_{"off"};
+
   float last_valid_{NAN};
   uint32_t last_valid_ms_{0};
 
   static const uint8_t SEGMENT_PATTERNS_[10];
   static const uint8_t DASH_BITMASK_ = 0b1000000;  // segment g only
   static const uint8_t ALL_ON_MIN_ = 100;           // min brightness when all segments lit (digit 8)
+};
+
+template<typename... Ts>
+class TriggerMeasurementAction : public Action<Ts...> {
+ public:
+  explicit TriggerMeasurementAction(DigitNumber *parent) : parent_(parent) {}
+  void play(Ts... x) override { parent_->do_trigger_(); }
+ private:
+  DigitNumber *parent_;
 };
 
 }  // namespace digit_number
